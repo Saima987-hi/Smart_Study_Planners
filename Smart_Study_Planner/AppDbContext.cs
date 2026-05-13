09863041a7b1c6f9c90ad75f96ad6e7e1f21b3b0
Saima987-hi <247276@students.au.edu.pk>
2026-05-13 20:55:21 +0500

    Add AppDbContext with DbSets for core domain models
    Introduced the AppDbContext class in the SmartStudyPlanner.Data namespace, inheriting from Entity Framework Core's DbContext. Defined DbSet properties for User, StudyTask, Subject, Exam, ScheduleEntry, PomodoroSession, and StudyLog entities to enable database management. Overrode OnModelCreating for future model configuration.

---
+++ Smart_Study_Planner/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using global::SmartStudyPlanner.Models;
+using Microsoft.EntityFrameworkCore;
+using SmartStudyPlanner.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace SmartStudyPlanner.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<StudyTask> StudyTasks => Set<StudyTask>();
+        public DbSet<Subject> Subjects => Set<Subject>();
+        public DbSet<Exam> Exams => Set<Exam>();
+        public DbSet<ScheduleEntry> ScheduleEntries => Set<ScheduleEntry>();
+        public DbSet<PomodoroSession> PomodoroSessions => Set<PomodoroSession>();
+        public DbSet<StudyLog> StudyLogs => Set<StudyLog>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
